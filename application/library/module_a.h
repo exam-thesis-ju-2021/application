668a270fed1fac245aabe60b74872d51e9a1a68a
Warcaith <f.v.carlsson@gmail.com>
2021-04-14 14:38:14 +0200

    feat: added simulated hardware incl. peripherals

---
+++ application/library/module_a.h
@@ -0,0 +1,11 @@
+#ifndef MODULE_A
+#define MODULE_A
+
+void initialize_hardware();
+void reset_hardware();
+double divide_by_half(double value);
+double multiply_by_two(double value);
+double power_of_two(double value);
+double square_root(double value);
+
+#endif
---
+++ application/main.c
@@ -0,0 +1,16 @@
+﻿#include "library/module_a.h"
+#include <stdio.h>
+
+int main()
+{
+	initialize_hardware();
+
+	printf("%f\n", divide_by_half(128));
+	printf("%f\n", multiply_by_two(128));
+	printf("%f\n", power_of_two(128));
+	printf("%f\n", square_root(128));
+
+	reset_hardware();
+
+	return 0;
+}
